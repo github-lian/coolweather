f3b6146c0bff0aa5ddc652c9887490c7546708cd
lian <1364332674@qq.com>
2019-05-25 10:55:21 +0800

    加入创建数据库和表以及各项配置

--- app/src/main/java/lian/coolweather/com/coolweather/MainActivity.java
+++ app/src/main/java/lian/coolweather/com/coolweather/MainActivity.java
@@ -1,16 +1,25 @@
 package lian.coolweather.com.coolweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvHello)
+    TextView mTvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
-    private void getData(){
+
+    private void getData() {
 
     }
 }
